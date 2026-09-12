e7962b28d8012b28946794f1012f0f252a698ee3
Devansh Arora <hsnaved.reverse@gmail.com>
2026-09-13 02:36:53 +0530

    feat: lower subwoofer amp gain

--- speakers.sh
+++ speakers.sh
@@ -1,3 +1,3 @@
 sudo hda-verb /dev/snd/hwC1D0 0x17 SET_PIN_WIDGET_CONTROL 0x40
 sudo hda-verb /dev/snd/hwC1D0 0x17 SET_AMP_GAIN_MUTE 0xb000
-sudo hda-verb /dev/snd/hwC1D0 0x02 SET_AMP_GAIN_MUTE 0xb057
+sudo hda-verb /dev/snd/hwC1D0 0x02 SET_AMP_GAIN_MUTE 0xb047
